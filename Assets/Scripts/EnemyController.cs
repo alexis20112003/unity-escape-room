cc3fed25b1ca665956e05e961bf1e2d339ddf181
User <User@Mira-PC-22-84.Aflokkat.lan>
2026-01-15 12:01:42 +0100

    change

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     private Transform _playerCamera;
+    public Rigidbody rigidbody;
+    public float vitesse;
+    public float distanceToStop;
 
     private void Start() {
         _playerCamera = Camera.main.transform;
@@ -13,5 +16,14 @@ public class EnemyController : MonoBehaviour
         directionToPlayer.y = 0;
 
         transform.rotation = Quaternion.LookRotation(directionToPlayer);
+
+        Vector3 movement = directionToPlayer.normalized * vitesse;
+        rigidbody.linearVelocity = movement + new Vector3(0, rigidbody.linearVelocity.y, 0);
+
+        //float distanceToStop = Vector3.Distance(transform.position, _playerCamera.position);
+        //if (distanceToStop > directionToPlayer)
+        //{
+            //rigidbody.linearVelocity = new Vector3(0, rigidbody.linearVelocity.y, 0);
+        //} 
     }
 }
